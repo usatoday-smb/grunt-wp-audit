f140983992072cbdec97c45311271866ccaf9f6a
Ephraim Gregor <egregor@usatoday.com>
2015-12-30 13:36:25 -0500

    Adds script to install dependencies.

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Installs PHP dependencies.
+
+if ! hash php 2>/dev/null; then
+	echo 'PHP not installed! Aborting.'
+	exit 1
+else
+	echo 'PHP installed.'
+fi
+
+DRY_RUN="$npm_config_dry_run"
+
+echo 'Making php directory.'
+if [ -z "$DRY_RUN" ]; then
+	mkdir php
+fi
+
+PHPCS_URL='https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar'
+echo 'Downloading PHPCS.'
+if [ -z "$DRY_RUN" ]; then
+	curl -o php/phpcs $PHPCS_URL
+else
+	echo "Downloaded PHPCS to ${pwd}/php/phpcs."
+fi
+
+WPCS_URL='https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git'
+echo 'Downloading WPCS.'
+if [ -z "$DRY_RUN" ]; then
+	git clone -b master $WPCS_URL php/wpcs
+else
+	echo "Cloned WP standards into ${pwd}/php/wpcs."
+fi
+
+echo 'Configuring PHPCS to use the WP standard.'
+if [ -z "$DRY_RUN" ]; then
+	php php/phpcs --config-set installed_paths php/wpcs
+fi
+
+echo 'Done!'
