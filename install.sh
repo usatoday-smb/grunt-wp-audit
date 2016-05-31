a890d1c9866d193ad70cdd20d0e70fc2bb65fd9f
Ephraim Gregor <egregor@usatoday.com>
2016-05-31 14:52:32 -0400

    Fixes permission issue, and allows smarter updating of script.

--- install.sh
+++ install.sh
@@ -9,30 +9,33 @@ else
 	echo 'PHP installed.'
 fi
 
+DIR="$(pwd)/php"
+
 echo 'Making php directory.'
-if [ -z "$DRY_RUN" ]; then
-	mkdir php
+if [ -z "$DRY_RUN" -a ! -d $DIR ]; then
+	mkdir $DIR
 fi
 
 PHPCS_URL='https://squizlabs.github.io/PHP_CodeSniffer/phpcs.phar'
 echo 'Downloading PHPCS.'
-if [ -z "$DRY_RUN" ]; then
-	curl -o php/phpcs $PHPCS_URL
-else
-	echo "Downloaded PHPCS to ${pwd}/php/phpcs."
+if [ -z "$DRY_RUN" -a ! -r $DIR/phpcs ]; then
+	curl -o $DIR/phpcs $PHPCS_URL
+	chmod +x $DIR/phpcs
+elif [ -n "$DRY_RUN" ]; then
+	echo "Downloaded PHPCS to $DIR/phpcs."
 fi
 
 WPCS_URL='https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards.git'
 echo 'Downloading WPCS.'
-if [ -z "$DRY_RUN" ]; then
-	git clone -b master $WPCS_URL php/wpcs
+if [ -z "$DRY_RUN" -a ! -d $DIR/wpcs ]; then
+	git clone -b master $WPCS_URL $DIR/wpcs
 else
-	echo "Cloned WP standards into ${pwd}/php/wpcs."
+	echo "Cloned WP standards into $DIR/wpcs."
 fi
 
 echo 'Configuring PHPCS to use the WP standard.'
 if [ -z "$DRY_RUN" ]; then
-	php php/phpcs --config-set installed_paths ${pwd}/php/wpcs
+	php php/phpcs --config-set installed_paths $DIR/wpcs
 fi
 
 echo 'Done!'
